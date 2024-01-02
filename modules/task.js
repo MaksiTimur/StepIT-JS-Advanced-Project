29ec1aa348f452052786c31e818802095030ab6a
Maksimov Timur <144324698+MaksiTimur@users.noreply.github.com>
2024-01-02 13:38:07 +0400

    Implemented class Task

--- modules/task.js
+++ modules/task.js
@@ -1,5 +1,22 @@
 export default class Task {
-    constructor() {
+    #status = false;
+    #id;
+    #date;
+    #name;
+    #description;
 
+    constructor(name, description) {
+        this.#id = Math.random().toString(16).slice(2);
+        this.#date = Task.#getDate();
+
+        this.#name = name;
+        this.#description = description;
+    }
+
+    static #getDate() {
+        const date = new Date().toLocaleString();
+        const splittedDate = date.split(', ');
+
+        return `${splittedDate[0]} ${splittedDate[1]}`;
     }
 }
